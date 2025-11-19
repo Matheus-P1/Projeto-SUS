b787f97e1db64065c640f7201a39a5d6b86ff564
Matheus-P1 <mfilipeguedes@gmail.com>
2025-11-19 05:49:41 -0300

    Update
    minor changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'meuApp',
+  appName: 'SUS App',
   webDir: 'www'
 };
 
