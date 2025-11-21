aad60b9ff02440939fd9d3133ef949a4ec848096
Matheus-P1 <mfilipeguedes@gmail.com>
2025-11-21 08:56:16 -0300

    icone update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,21 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'SUS App',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+      backgroundColor: "#ffffff",
+      showSpinner: false,
+      androidScaleType: "CENTER_CRP",
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
