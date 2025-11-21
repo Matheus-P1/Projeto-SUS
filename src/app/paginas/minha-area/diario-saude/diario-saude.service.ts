2f317bcd0eff05c26cd8732f4915fad0411e6258
Matheus-P1 <mfilipeguedes@gmail.com>
2025-11-20 21:59:47 -0300

    diario

--- src/app/paginas/minha-area/diario-saude/diario-saude.service.ts
+++ src/app/paginas/minha-area/diario-saude/diario-saude.service.ts
@@ -39,15 +39,32 @@ export class DiarioSaudeService {
 
   addRegistroPressao(dados: RegistroPressao) {
     this.registrosPressao.push(dados);
-    console.log('Serviço: Novo registro de PRESSÃO adicionado!', dados);
   }
   addRegistroGlicose(dados: RegistroGlicose) {
     this.registrosGlicose.push(dados);
-    console.log('Serviço: Novo registro de GLICOSE adicionado!', dados);
   }
   addRegistroIMC(dados: RegistroIMC) {
     this.registrosIMC.push(dados);
-    console.log('Serviço: Novo registro de IMC adicionado!', dados);
   }
 
+  removeRegistroPressao(dados: RegistroPressao) {
+    const index = this.registrosPressao.indexOf(dados);
+    if (index > -1) {
+      this.registrosPressao.splice(index, 1);
+    }
+  }
+
+  removeRegistroGlicose(dados: RegistroGlicose) {
+    const index = this.registrosGlicose.indexOf(dados);
+    if (index > -1) {
+      this.registrosGlicose.splice(index, 1);
+    }
+  }
+
+  removeRegistroIMC(dados: RegistroIMC) {
+    const index = this.registrosIMC.indexOf(dados);
+    if (index > -1) {
+      this.registrosIMC.splice(index, 1);
+    }
+  }
 }
