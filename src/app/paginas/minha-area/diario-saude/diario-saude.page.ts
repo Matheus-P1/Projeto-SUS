21f3cd82cbd0aeddd9d97bf9e06eac070f831168
Matheus-P1 <mfilipeguedes@gmail.com>
2025-11-21 07:01:55 -0300

    Update
    arquivos e update em diario-saude

--- src/app/paginas/minha-area/diario-saude/diario-saude.page.ts
+++ src/app/paginas/minha-area/diario-saude/diario-saude.page.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ModalController } from '@ionic/angular/standalone';
+import { ModalController, AlertController } from '@ionic/angular/standalone'; 
 import { DiarioSaudeService, RegistroPressao, RegistroGlicose, RegistroIMC } from './diario-saude.service';
+import { addIcons } from 'ionicons';
+import { addCircle, trashOutline } from 'ionicons/icons';
 
 import { PressaoModalComponent } from '../../../componentes/pressao-modal/pressao-modal.component';
 import { GlicoseModalComponent } from '../../../componentes/glicose-modal/glicose-modal.component';
 import { ImcModalComponent } from '../../../componentes/imc-modal/imc-modal.component';
 import { SimpleHeaderModule } from 'src/app/componentes/simple-header/simple-header.module';
-
 import {
   IonContent, 
   IonSegment, 
@@ -38,28 +39,28 @@ import {
     IonCol, 
     IonCard, 
     IonIcon,
-    //PressaoModalComponent, 
-    //GlicoseModalComponent,
-    //ImcModalComponent,
     SimpleHeaderModule
   ]
 })
 export class DiarioSaudePage implements OnInit {
 
-  segmentoAtivo: string = 'pressao'; 
-  
+  segmentoAtivo: string = 'pressao';
   registrosPressao: RegistroPressao[] = [];
   registrosGlicose: RegistroGlicose[] = [];
   registrosIMC: RegistroIMC[] = [];
 
   constructor(
     private modalCtrl: ModalController,
-    private diarioService: DiarioSaudeService
-  ) { }
+    private diarioService: DiarioSaudeService,
+    private alertController: AlertController
+  ) { 
+    addIcons({ addCircle, trashOutline });
+  }
 
   ngOnInit() {
     this.carregarDados();
   }
+
   carregarDados() {
     this.registrosPressao = this.diarioService.getRegistrosPressao();
     this.registrosGlicose = this.diarioService.getRegistrosGlicose();
@@ -70,23 +71,57 @@ export class DiarioSaudePage implements OnInit {
     this.segmentoAtivo = event.detail.value;
   }
 
+  
+  async confirmarExclusao(registro: any, tipo: 'pressao' | 'glicose' | 'imc') {
+    const alert = await this.alertController.create({
+      header: 'Excluir Registro',
+      message: 'Tem certeza que deseja remover este registro?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        },
+        {
+          text: 'Excluir',
+          role: 'destructive',
+          handler: () => {
+            this.deletarRegistro(registro, tipo);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  deletarRegistro(registro: any, tipo: string) {
+    if (tipo === 'pressao') {
+        this.diarioService.removeRegistroPressao(registro);
+    } 
+    else if (tipo === 'glicose') {
+        this.diarioService.removeRegistroGlicose(registro);
+    }
+    else if (tipo === 'imc') {
+        this.diarioService.removeRegistroIMC(registro);
+    }
+
+    this.carregarDados();
+  }
+
+
   async abrirModalAdicionar() {
     let modalComponent;
-    
     if (this.segmentoAtivo === 'pressao') {
       modalComponent = PressaoModalComponent;
     } 
     else if (this.segmentoAtivo === 'glicose') {
-      console.log('Abrir modal para Glicose...');
       modalComponent = GlicoseModalComponent;
     } 
     else if (this.segmentoAtivo === 'imc') {
-      console.log('Abrir modal para IMC...');
       modalComponent = ImcModalComponent;
     }
 
     if (!modalComponent) {
-      console.warn('Modal para este segmento ainda não implementado:', this.segmentoAtivo);
       return;
     }
 
@@ -96,10 +131,11 @@ export class DiarioSaudePage implements OnInit {
       initialBreakpoint: 0.6,
       handle: true,
     });
+
     await modal.present();
 
     const { data, role } = await modal.onDidDismiss();
-    
+
     if (role === 'adicionar') {
       if (this.segmentoAtivo === 'pressao') {
         this.diarioService.addRegistroPressao(data);
